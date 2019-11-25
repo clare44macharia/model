ccfd259d49fd173463c80c39d0423df777b33c38
clare <anastasia.macharia@strathmore.edu>
2019-11-25 16:32:24 +0300

    changes in json return

--- model/app.py
+++ model/app.py
@@ -26,9 +26,9 @@ def predict():
 			query = query.reindex(columns=model_columns, fill_value=0)
 
 			print(query)
-			prediction = lr.predict(query).round()
+			prediction = lr.predict(query).round().astype(int)
 
-			return jsonify({'prediction': str(prediction[0])})
+			return jsonify({'prediction': str(prediction[0][0])})
 		except:
 			return jsonify({'trace': traceback.format_exc()})
 			#K.clear_session()	
